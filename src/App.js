1e2dc5288d0984a08a2f742e55ad888886c5e48d
masnurrm <sangmastahnm@gmail.com>
2024-02-21 17:08:28 +0700

    feat: adding main layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-export default function App() {
+function App() {
     return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
+        <div className={`flex flex-grow flex-col bg-primary-color transition-colors duration-500 ease-in-out`}>
+            <div className="flex-grow content max-w-[38.75rem] mx-auto px-3 font-light flex flex-col">
+            </div>
+        </div>
+      );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundColor: {
+            'btn-color': 'var(--btn-color)',
+            'primary-color': 'var(--primary-color)',
+            'secondary-color': 'var(--secondary-color)',
+            'tertiary-color': 'var(--tertiary-color)',
+        },
+        borderColor: {
+            'btn-color': 'var(--btn-color)',
+        }
+      },
     },
     plugins: [],
   };
